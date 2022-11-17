e1ec40c7d64042769de7e39f8b110a0c4237d413
john.agudelo <johnaagudelo@gmail.com>
2022-11-17 08:53:34 -0500

    implement sidebar

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menu = document.querySelector('.desktop-menu');
+
 const menuBurgerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const ButtonShoppingCart = document.querySelector('.navbar-shopping-card');
+const DetailShoppingCart = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', () => toggleMenu(menu));
 menuBurgerIcon.addEventListener('click', () => toggleMenu(mobileMenu));
+ButtonShoppingCart.addEventListener('click', () =>
+  toggleMenu(DetailShoppingCart)
+);
 
 function toggleMenu(element) {
   element.classList.toggle('inactive');
