8d97383588030ccee8ffd7fbdfc83646e2ba2923
john.agudelo <johnaagudelo@gmail.com>
2022-12-21 19:25:08 +0100

    add to implement for opening and close porduct detail

--- main.js
+++ main.js
@@ -6,12 +6,19 @@ const mobileMenu = document.querySelector('.mobile-menu');
 
 const ButtonShoppingCart = document.querySelector('.navbar-shopping-card');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailtContainer = document.querySelector('#productDetail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 
 menuEmail.addEventListener('click', () => toggleMenu(menu));
 menuBurgerIcon.addEventListener('click', () => toggleMenu(mobileMenu));
-ButtonShoppingCart.addEventListener('click', () =>
-  toggleMenu(shoppingCartContainer)
-);
+ButtonShoppingCart.addEventListener('click', () => {
+  productDetailtContainer.classList.add('inactive');
+  toggleMenu(shoppingCartContainer);
+});
+
+productDetailCloseIcon.addEventListener('click', () => {
+  productDetailtContainer.classList.add('inactive');
+});
 
 function toggleMenu(element) {
   element.classList.toggle('inactive');
@@ -57,55 +64,66 @@ const productList = [
 </div> */
 }
 
-const containerCard = document.querySelector('.cards-container');
-for (product of productList) {
-  const productCart = document.createElement('div');
-  productCart.classList.add('product-card');
-
-  const imgCart = document.createElement('img');
-  imgCart.setAttribute('src', product.image);
-  imgCart.classList.add('product-img');
-
-  const divProductInfo = document.createElement('div');
-  divProductInfo.classList.add('product-info');
-
-  const divWrapperInfo = document.createElement('div');
-
-  const pProductPrice = document.createElement('p');
-  pProductPrice.innerText = product.price;
-
-  const pProductName = document.createElement('p');
-  pProductName.innerText = product.name;
-
-  const figureWrapper = document.createElement('figure');
-
-  const imgAddCard = document.createElement('img');
-  imgAddCard.setAttribute('src', './icons/bt_add_to_cart.svg');
-
-  figureWrapper.appendChild(imgAddCard);
-
-  divWrapperInfo.append(pProductPrice, pProductName);
-
-  divProductInfo.append(divWrapperInfo, figureWrapper);
-
-  productCart.append(imgCart, divProductInfo);
-
-  // productCart.innerHTML = `
-  //   <img
-  //     src="${product.image}"
-  //     alt=""
-  //     class="product-img"
-  //   />
-  //   <div class="product-info">
-  //     <div>
-  //       <p>${product.price}</p>
-  //       <p>${product.name}</p>
-  //     </div>
-  //     <figure>
-  //       <img src="./icons/bt_add_to_cart.svg" alt="add-card" />
-  //     </figure>
-  //   </div>
-  // `;
-
-  containerCard.appendChild(productCart);
+function openProductDetail() {
+  shoppingCartContainer.classList.add('inactive');
+  productDetailtContainer.classList.remove('inactive');
 }
+
+const containerCard = document.querySelector('.cards-container');
+
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCart = document.createElement('div');
+    productCart.classList.add('product-card');
+
+    const productImage = document.createElement('img');
+    productImage.setAttribute('src', product.image);
+    productImage.classList.add('product-img');
+    productImage.addEventListener('click', openProductDetail);
+
+    const divProductInfo = document.createElement('div');
+    divProductInfo.classList.add('product-info');
+
+    const divWrapperInfo = document.createElement('div');
+
+    const pProductPrice = document.createElement('p');
+    pProductPrice.innerText = `$ ${product.price}`;
+
+    const pProductName = document.createElement('p');
+    pProductName.innerText = product.name;
+
+    const figureWrapper = document.createElement('figure');
+
+    const imgAddCard = document.createElement('img');
+    imgAddCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    figureWrapper.appendChild(imgAddCard);
+
+    divWrapperInfo.append(pProductPrice, pProductName);
+
+    divProductInfo.append(divWrapperInfo, figureWrapper);
+
+    productCart.append(productImage, divProductInfo);
+
+    // productCart.innerHTML = `
+    //   <img
+    //     src="${product.image}"
+    //     alt=""
+    //     class="product-img"
+    //   />
+    //   <div class="product-info">
+    //     <div>
+    //       <p>${product.price}</p>
+    //       <p>${product.name}</p>
+    //     </div>
+    //     <figure>
+    //       <img src="./icons/bt_add_to_cart.svg" alt="add-card" />
+    //     </figure>
+    //   </div>
+    // `;
+
+    containerCard.appendChild(productCart);
+  }
+}
+
+renderProducts(productList);
