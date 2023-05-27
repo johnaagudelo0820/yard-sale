5f827b18f915eb6ec790fc999a6fee2fe89f2d16
john.agudelo <johnaagudelo@gmail.com>
2023-05-27 11:39:48 -0500

    ✨ crear nuevo componente de ProductItem

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+import addCard from '@assets/icons/bt_add_to_cart.svg';
+
+const ProductItem = () => {
+  const [cart, setCart] = useState([]);
+
+  const handlerCart = () => {
+    setCart([]);
+  };
+
+  return (
+    <div className="ProductItem">
+      <img
+        src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+        className="product-img"
+      />
+      <div className="product-info">
+        <div>
+          <p>120,00</p>
+          <p>Bike</p>
+        </div>
+        <figure onClick={handlerCart}>
+          <img src={addCard} alt="add-card" />
+        </figure>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
