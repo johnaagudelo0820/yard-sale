c39c05558bedcf205be57fb178702e9681dbc04c
john.agudelo <johnaagudelo@gmail.com>
2023-06-04 13:51:00 -0500

    ✨ obtener todos los productos

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductItem from '../components/ProductItem';
 
 import '@styles/ProductList.scss';
 
+const API = `http://api.escuelajs.co/api/v1/products`;
+
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  useState(() => {
+    const getProducts = async () => {
+      const response = await fetch(API);
+      const data = await response.json();
+      setProducts(data);
+    };
+    getProducts();
+  }, []);
+
   return (
     <section className="main-container">
       <div className="cards-container">
-        <ProductItem />
+        {products.map((product) => (
+          <ProductItem key={product.id} />
+        ))}
       </div>
     </section>
   );
